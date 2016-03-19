2456aca6b76cd22b9de1ce66392ff50646ae4e07
Firedrake969 <firedrake969@users.noreply.github.com>
2016-03-18 22:11:25 -0600

    switch to synapses

--- main.go
+++ main.go
@@ -13,24 +13,31 @@ import (
     * is "value at address"
 */
 
+type Connection struct {
+    Strength float64 `json:"strength"`
+    From *Node       `json:"-"`
+    // To *Node         `json:"-"`
+}
+
 type Node struct {
-    Value float64        `json:"value"`  // should this be float32?  idk
-    Connections []*Node  `json:"connections"`  //which nodes to read from
-    Level int            `json:"level"`  // for 
+    Value float64             `json:"value"`  // should this be float32?  idk
+    Connections []*Connection  `json:"connections"`  //which nodes to read from
+    Level int                  `json:"level"`  // for 
 }
 
 type Network struct {
     InputNodes []*Node   `json:"inputNodes"`
     Nodes []*Node        `json:"nodes"`
     OutputNodes []*Node  `json:"outputNodes"`
+    MaxLevel int         `json:"maxLevel"`
     MaxCycles int        `json:"maxCycles"`
     CurCycle int         `json:"curCycle"`
 }
 
 func (n *Node) update() {
     // figure out how to do this
-    // two approaches - all connect all, or a random X number of connections
-    // random would probably be a little less predictable, so probably better to do random
+    // synapses should strengthen when used and weaken when not - maybe multiply by .9 and 1.1 or something?
+    // go by levels - so it can kinda radiate outwards
 }
 
 func (n Node) String() string {
@@ -40,6 +47,16 @@ func (n Node) String() string {
 
 func (net *Network) connect() {
     // do things?
+    // first connect all "body" nodes to "input"
+    for _, bodyNode := range net.Nodes {
+        bodyNode.Connections = append(bodyNode.Connections, &Connection{
+            Strength: 0.5,
+        })
+    }
+    // then connect all "body" nodes to the ones on the same level
+
+    // then connect all "output" nodes to "body"
+
 }
 
 func (net *Network) cycle() {
@@ -54,7 +71,7 @@ func (net Network) String() string {
     return string(jsonRep)
 }
 
-func MakeNetwork(input, processing, output, cycles, perLevel int) *Network {
+func MakeNetwork(input, processing, output, cyclesPerLevel, perLevel int) *Network {
     nodes := []*Node{}
     inputNodes := []*Node{}
     outputNodes := []*Node{}
@@ -68,7 +85,6 @@ func MakeNetwork(input, processing, output, cycles, perLevel int) *Network {
     for i := 1; i <= processing; i++ {
         nodes = append(nodes, &Node{
             Value: 0,
-            Connections: []*Node{},
             Level: curLevel,
         })
         if math.Mod(float64(i), float64(perLevel)) == 0 {
@@ -85,15 +101,16 @@ func MakeNetwork(input, processing, output, cycles, perLevel int) *Network {
         Nodes: nodes,
         InputNodes: inputNodes,
         OutputNodes: outputNodes,
+        MaxLevel: curLevel,
         CurCycle: 0,
-        MaxCycles: cycles,
+        MaxCycles: cyclesPerLevel,
     }
 }
 
 func main() {
-    // input, processing, output, cycles, perLevel
+    // input, processing, output, cyclesPerLevel, perLevel
     myNet := MakeNetwork(1, 5, 1, 25, 2)
     myNet.connect()
-    myNet.cycle()
+    // myNet.cycle()
     fmt.Println(myNet)
 }
