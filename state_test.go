120032ff286c70b7e98177af39ea3e76a6944a66
Firedrake969 <firedrake969@users.noreply.github.com>
2016-10-16 15:54:37 -0600

    add sensor before running iterations

--- state_test.go
+++ state_test.go
@@ -12,16 +12,16 @@ func TestState(t *testing.T) {
     testingNet.Mirror()
     testingNet.ConnectHemispheres()
 
-    // this is to make sure adding/removing connections works
-    for i := 0; i < 100; i++ {
-        testingNet.Cycle()
-    }
-
     testingNet.CreateSensor("eye", 1, 9, "y", [3]int{8, 0, 12}, 2, func(nodes []*Node, influences map[string]*Output) {
         for _, node := range nodes {
             node.Value = 1
         }
     })
+
+    // this is to make sure adding/removing connections works
+    for i := 0; i < 100; i++ {
+        testingNet.Cycle()
+    }
 
     testingNet.SaveState("test")
     loadedNet := LoadState("test")
