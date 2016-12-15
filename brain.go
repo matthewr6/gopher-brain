b9bdc6f59d7099732926d08a92421983cf735c2b
Firedrake969 <firedrake969@users.noreply.github.com>
2016-12-14 20:15:24 -0700

    move some stuff out of package

--- brain.go
+++ brain.go
@@ -6,7 +6,6 @@ import (
     "time"
     "bufio"
     "strings"
-    "strconv"
     "math/rand"
 )
 
@@ -27,7 +26,7 @@ type SensorConstructor struct {
     InputFunc func([]*Node, map[string]*Output)
 }
 
-func Brain(NETWORK_SIZE [3]int, CONSTRUCTORS []SensorConstructor) {
+func Brain(NETWORK_SIZE [3]int, CONSTRUCTORS []SensorConstructor) *Network {
     reader := bufio.NewReader(os.Stdin)
     // todo - directory name to load state from
     fileName := Prompt("Enter state name to load state, or leave blank to create a new network:  ", reader)
@@ -44,49 +43,23 @@ func Brain(NETWORK_SIZE [3]int, CONSTRUCTORS []SensorConstructor) {
     } else {
         myNet = LoadState(fileName)
     }
-    var mode string
     tracker := make(map[string]bool)
     fmt.Printf("Currently has %v sensors and %v outputs.\n", len(myNet.Sensors), len(myNet.Outputs))
-    fmt.Println("Sensor names:")
-    for name := range myNet.Sensors {
-        baseName := strings.Split(name, "-")[0]
-        if !tracker[baseName] {
-            fmt.Println(baseName)
+    if len(myNet.Sensor) > 0 {
+        fmt.Println("Sensor names:")
+        for name := range myNet.Sensors {
+            baseName := strings.Split(name, "-")[0]
+            if !tracker[baseName] {
+                fmt.Println(baseName)
+            }
+            tracker[baseName] = true
         }
-        tracker[baseName] = true
     }
-    mode = Prompt("Add/modify the custom things? [y/n]  ", reader)
-    if mode == "y" {
-        fmt.Println("WARNING!  Sensors and outputs will not save properly!")
-        // let's pretend the front x/z plane (y = 1) is "front" with left being x = 25
-        // maybe you should only create sensors, and specify # of corresponding outputs - and then the createSensor generates the outputs automatically
-        for _, constructor := range CONSTRUCTORS {
-            myNet.CreateSensor(constructor.Name, constructor.R, constructor.Count, constructor.Plane, constructor.Center, constructor.OutputCount, constructor.InputFunc)
-        }
+    for _, constructor := range CONSTRUCTORS {
+        myNet.CreateSensor(constructor.Name, constructor.R, constructor.Count, constructor.Plane, constructor.Center, constructor.OutputCount, constructor.InputFunc)
     }
     myNet.PruneUnusedSensors()
     fmt.Printf("Now has %v sensors and %v outputs.\n", len(myNet.Sensors), len(myNet.Outputs))
 
-    framesInput := Prompt("Enter number of frames, or leave blank to run until manually stopped:  ", reader)
-    frames, err := strconv.Atoi(framesInput)
-    if err != nil {
-        frames = 0
-    }
-
-    directory = Prompt("Enter directory to save frames and state to:  ", reader)
-    if directory == "" {
-        directory = "."
-    }
-    if directory[len(directory)-1] == '/' {
-        directory = directory[0:len(directory)-1]
-    }
-    
-    myNet.GenerateAnim(frames)
-
-    fmt.Print("\nSave state?  Enter a name if you wish to save the state:  ")
-    fileName, _ = reader.ReadString('\n')
-    fileName = strings.TrimSpace(fileName)
-    if fileName != "" {
-        myNet.SaveState(fileName)
-    }
+    return myNet
 }
--- example/main.go
+++ example/main.go
@@ -1,10 +1,20 @@
 package main
 
 // import "github.com/firedrake969/gopher-brain"
-import ".."
+import (
+    ".."
+
+    "os"
+    "fmt"
+    "bufio"
+    "strings"
+    "strconv"
+)
 
 func main() {
-    brain.Brain([3]int{12, 25, 25}, []brain.SensorConstructor{
+    reader := bufio.NewReader(os.Stdin)
+
+    myNet := brain.Brain([3]int{12, 25, 25}, []brain.SensorConstructor{
         brain.SensorConstructor{
             Name:"eye",
             R: 1,
@@ -19,4 +29,27 @@ func main() {
             },
         },
     })
+
+    framesInput := brain.Prompt("Enter number of frames, or leave blank to run until manually stopped:  ", reader)
+    frames, err := strconv.Atoi(framesInput)
+    if err != nil {
+        frames = 0
+    }
+
+    directory := brain.Prompt("Enter directory to save frames and state to:  ", reader)
+    if directory == "" {
+        directory = "."
+    }
+    if directory[len(directory)-1] == '/' {
+        directory = directory[0:len(directory)-1]
+    }
+    
+    myNet.GenerateAnim(frames)
+
+    fmt.Print("\nSave state?  Enter a name if you wish to save the state:  ")
+    fileName, _ := reader.ReadString('\n')
+    fileName = strings.TrimSpace(fileName)
+    if fileName != "" {
+        myNet.SaveState(fileName)
+    }
 }
